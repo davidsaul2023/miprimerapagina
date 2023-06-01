579e6b31971755aa575db5eb363f0c1685a6fc6e
davidsaul2023 <davidestebansaul@gmail.com>
2023-06-01 20:26:25 -0300

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+
+
+document
+  .querySelector("button.button-menu-toggle")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".nav-links")
+      .classList.toggle("nav-links-responsive");
+  });
+
+  function showAlert() {
+    alert("Esto es una alerta Javascript");
+  }
